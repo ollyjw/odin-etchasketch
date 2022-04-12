7b4ec6e761d76ec8320052d63a874fdaf073dc9d
ollyjw <owilcocks@gmail.com>
2022-04-12 12:36:06 +0100

    created resize grid function

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 const container = document.querySelector('.container');
 const newDiv = document.createElement("div");
-let gridSize = 16;
+// let gridSize = 16;
 
 const clearBtn = document.getElementById('clear');
 const resizeBtn = document.getElementById('resize');
 
+createGrid(16);
 
 // Create 16x16 grid of divs with square class
-function createGrid() {
+function createGrid(gridSize) {
 
-  // container.style.gridTemplateColumns = `repeat(${userSelection, 1fr})`;
-  // container.style.gridTemplateRows = `repeat(${userSelection, 1fr})`;
-  
   for (i = 1; i < gridSize * gridSize; i++) {
+    // add div with square class to container
     container.appendChild(newDiv);
     newDiv.className = 'square';
     let cloneDiv = newDiv.cloneNode();
@@ -25,22 +24,53 @@ function createGrid() {
   const squaresArray = Array.from(square);
 
   // for each item in the array, add function with new declared parameter of 'sqr' (square)
-  squaresArray.forEach(function(sqr) {
-
-    // Hover effect - grid adds bg color class as mouse passes over
+  squaresArray.forEach(function (sqr) {
+    // Hover effect - grid adds bg color class to each item (div with square class) as mouse passes over
     sqr.addEventListener('mouseenter', e => {
       sqr.classList.add("bg-dark-grey");
     })
+  });
 
-    // click a clear button and it clears the current grid - think i wanna put this in a function of its own so I can use it on the resize button too?
+  // - click clear button and remove bg color class
+  clearBtn.addEventListener('click', e => {
+    clearGrid();
+  })
 
-    // - click clear button and remove class
-    clearBtn.addEventListener('click', e => {
-      sqr.classList.remove("bg-dark-grey");
-    })
+}
 
+// remove dark grey bg color for squares
+function clearGrid() {
+  const square = document.getElementsByClassName('square');
+  const squaresArray = Array.from(square);
 
+  squaresArray.forEach(function (sqr) {
+    sqr.classList.remove("bg-dark-grey");
   });
 }
 
-createGrid();
+// clicking  resize button prompts you for a new grid number of squares per side - set max no. to 100
+
+resizeBtn.addEventListener("click", e => {
+  let newGridSize = prompt("How many squares per side? Please enter number between 1 & 100");
+  // parseInt() function parses a string argument and returns an integer or NaN
+  let newGridSizeInt = parseInt(newGridSize);
+
+  // while nums arent between 1 & 100 keep prompting
+  while (newGridSizeInt < 1 || newGridSizeInt > 100) {
+    newGridSize = prompt("How many squares per side? Please enter number between 1 & 100");
+    newGridSizeInt = parseInt(newGridSize);
+  }
+
+  // NEED to change css property 'grid-template-columns' to change columns according to user input
+  container.style.gridTemplateColumns = `repeat(${newGridSizeInt}, auto)`;
+  // changed auto to 1fr because it produced uneven rows
+  container.style.gridTemplateRows = `repeat(${newGridSizeInt}, 1fr)`;
+
+  clearGrid();
+  
+  createGrid(newGridSizeInt);
+
+})
+
+// To fix:
+// - onclick of resize btn i need to delete the old divs as well as removing dark grey class
